8c8f48e967413cf3aba8f1732a916f912e741a21
yuwa <yuwamama.1213@gmail.com>
2022-10-17 12:15:22 +0900

    1017

--- app/Main.hs
+++ app/Main.hs
@@ -29,6 +29,12 @@ func :: String -> String
 -- func input = case readInt input of
 --     n -> showInt (n `mod` 5)
 
-func input = case words input of
-    s:_ -> unlines [concat (replicate 3 s)]
-    [] -> error "invalid input"
+-- func input = case words input of
+--     s:_ -> unlines [concat (replicate 3 s)]
+--     [] -> error "invalid input"
+
+-- func input = case map words (lines input) of
+--     [[s]] ->  unlines [[s !! 2]]
+
+func input = case map (map readInt . words) (lines input) of
+    [[x]] -> unlines [showInt (24 - x)]
